6b9f4b2afd4bedf0058f2224c76bf7a1ac0ab9e7
johndyer <johndyer@gmail.com>
2011-06-01 15:57:40 -0500

    - version update

--- Builder.py
+++ Builder.py
@@ -3,13 +3,14 @@ import os
 import shutil
 
 
+
 bibly_files = []
 bibly_files.append('bible.js')
 bibly_files.append('bible.reference.js')
 bibly_files.append('bibly.js')
 
 bibly_filename = 'bibly'
-bibly_version = '0.7.1'
+bibly_version = '0.8'
 bibly_version_path = 'build/' + bibly_version + '/'
 bibly_active_path = 'build/'
 
@@ -21,7 +22,7 @@ if os.path.exists(bibly_version_path) == 0:
 biblyjs = bibly_filename + '.js'
 biblyjs_min = bibly_filename + '.min.js'
 
-# combind files
+# combine files
 code = ''
 for item in bibly_files:
 	src_file = open(item,'r')
@@ -50,3 +51,37 @@ shutil.copy2(bibly_version_path + biblycss, bibly_active_path + biblycss)
 
 os.system('java -jar yuicompressor-2.4.2.jar ' + biblycss + ' -o ' + bibly_version_path + biblycss_min + ' --charset utf-8 -v')
 shutil.copy2(bibly_version_path + biblycss_min , bibly_active_path + biblycss_min)
+
+
+
+# PREPEND intros
+def addHeader(headerFilename, filename):
+
+	# get the header text
+	tmp_file = open(headerFilename)
+	header_txt = tmp_file.read();
+	tmp_file.close()
+
+	# read the current contents of the file
+	tmp_file = open(filename)
+	file_txt = tmp_file.read()
+	tmp_file.close()
+	
+	# open the file again for writing
+	tmp_file = open(filename, 'w')
+	tmp_file.write(header_txt)
+	# write the original contents
+	tmp_file.write(file_txt)
+	tmp_file.close()
+
+
+addHeader('bibly.copyright.js', bibly_version_path + biblyjs)
+addHeader('bibly.copyright.js', bibly_active_path + biblyjs)
+addHeader('bibly.copyright.js', bibly_version_path + biblyjs_min)
+addHeader('bibly.copyright.js', bibly_active_path + biblyjs_min)
+
+
+addHeader('bibly.copyright.css', bibly_version_path + biblycss)
+addHeader('bibly.copyright.css', bibly_active_path + biblycss)
+addHeader('bibly.copyright.css', bibly_version_path + biblycss_min)
+addHeader('bibly.copyright.css', bibly_active_path + biblycss_min)
